eb332a3bb87bc3a8265285a6d88f0fd5e89d1d6e
AHADBAVA <ahaddesgins@gmail.com>
2026-03-20 23:31:45 +0530

    Reapply "refactor(home): update homepage section flow"
    This reverts commit 498a1820c7c18af3d3cd5a945aeb27d49d2ff8fc.

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,17 @@ import Hero from "./components/Hero";
 import Mission from "./components/Mission";
 import Contributors from "./components/Contributors";
 import Projects from "./components/Projects";
-import Guidelines from "./components/Guidelines";
 import JoinCTA from "./components/JoinCTA";
 import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative z-10">
+    <main className="relative z-10 overflow-x-clip bg-kcc-bg pb-24 text-kcc-text md:pb-0">
       <NavBar />
       <Hero />
       <Mission />
       <Contributors />
       <Projects />
-      <Guidelines />
       <JoinCTA />
       <Footer />
     </main>
